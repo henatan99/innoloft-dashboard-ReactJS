d659bb2757e64f4270b6e9d2122b801ee4211398
Henok Mekonnen <henatan99@yahoo.com>
2022-04-27 08:49:32 -0400

    Add routing to Home and Product components with a layout context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Routes } from 'react-router';
+
 import Layout from './components/layout/layout';
+import Home from './components/Home/home';
 import Product from './components/product/product';
 
 function App() {
   return (
-    <Layout />
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/product" element={<Product />} />
+        </Routes>
+      </Layout>
+    </Router>
+
   );
 }
 
---
+++ src/components/Home/home.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => (
+  <div>This is Home</div>
+);
+
+export default Home;
